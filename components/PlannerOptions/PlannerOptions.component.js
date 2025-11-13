d6d7a05603ea30c33b527f5105818acb5d9960cd
Guilherme S. Santos <guilhermess1994@gmail.com>
2025-11-13 18:36:14 -0300

    adicionado componente `PlannerOptions`

---
+++ components/PlannerOptions/PlannerOptions.component.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import style from "./PlannerOptions.module.css";
+import { EllipsisVertical } from "lucide-react";
+
+function PlannerOptions(params) {
+  const [showOptions, setShowOptions] = useState(false);
+
+  function toggleOptions() {
+    setShowOptions(!showOptions);
+  }
+  return (
+    <>
+      <button onClick={toggleOptions} className={style.actionButton}>
+        <EllipsisVertical size="18px" />
+      </button>
+      {showOptions && (
+        <ul className={style.popmenu}>
+          <li>Exportar em pdf</li>
+          <li>Exportar em csv</li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export { PlannerOptions };
