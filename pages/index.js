ce7dc38cc385caa89ac5c2571d3eb5f4d70a2ae4
Guilherme S. Santos <guilhermess1994@gmail.com>
2025-11-13 18:36:48 -0300

    adicionado style nas páginas

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default function home() {
-    return (
-        <main style={{ padding: "2rem", fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial" }}>
-            <h1>Literatura diária</h1>
-            <p>Bem-vindo à página inicial.</p>
+  return (
+    <main
+      style={{
+        padding: "2rem",
+        fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial",
+      }}
+    >
+      <h1>Literatura diária</h1>
+      <p>Bem-vindo à página inicial.</p>
 
-            <p>Em breve uma descrição mais detalhada do projeto estará disponível aqui. </p>
-        </main>
-    );
+      <p>Em breve uma descrição mais detalhada do projeto estará disponível aqui. </p>
+    </main>
+  );
 }
--- pages/planner/index.js
+++ pages/planner/index.js
@@ -1,43 +1,27 @@
-import { useState } from "react";
+import style from "./planner.module.css";
+import { PlannerOptions } from "../../components/PlannerOptions/PlannerOptions.component";
 
-function Planner(){
+function Planner() {
+  return (
+    <div className={style.page}>
+      <PlannerOptions />
+      <h1 className={style.header}>Título</h1>
+      <h3>Autor:</h3>
+      <h5>Início: DD/MM/YYYY Fim: DD/MM/YYYY</h5>
+      <div>estrelas</div>
+      <p>Progresso: X%</p>
+      <p>Meta de hoje: X capítulos (X páginas)</p>
+      <p>Próxima meta: Dia X - Capítulos (páginas)</p>
 
-    const [showOptions, setShowOptions] = useState(false);
+      <div>
+        <button>Dia 1: Capítulos (páginas) - Concluir Meta</button>
 
-    function toggleOptions(){
-        setShowOptions(!showOptions);
-    }
-
-    return(<div>
-        
-        <h1>Título</h1>
-        <h3>Autor:</h3>
-        <h5>Início: DD/MM/YYYY Fim: DD/MM/YYYY</h5>
-        <div>estrelas</div>
-        <p>Progresso: X%</p>
-        <p>Meta de hoje: X capítulos (X páginas)</p>
-        <p>Próxima meta: Dia X - Capítulos (páginas)</p>
-
-        <button onClick={toggleOptions}>Opções</button>
-        {
-            showOptions &&
-            <ul>
-                <li>Exportar em pdf</li>
-                <li>Exportar em csv</li>
-            </ul>
-        }
-        <div>
-
-            <button>Dia 1: Capítulos (páginas) - Concluir Meta</button>
-
-            <ul>
-                <li>Dia 1: Prefácio (p.7-18)</li>
-            </ul>
-
-        </div>
-
-
-    </div>);
+        <ul>
+          <li>Dia 1: Prefácio (p.7-18)</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Planner;
