4af642dee6986503a680902016d1277bcee9a313
Guilherme Santos <guilhermess1994@gmail.com>
2025-11-16 01:50:00 +0000

    adding

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial" }}>
+      <h1>Literatura diária</h1>
+      <p>Bem-vindo à página inicial.</p>
+
+      <p>Em breve uma descrição mais detalhada do projeto estará disponível aqui. </p>
+    </main>
+  );
+};
+
+export default Home;
