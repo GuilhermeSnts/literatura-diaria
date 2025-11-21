7479b882e41f8dc121489289b8ab72e98bb50ce3
Guilherme Santos <guilhermess1994@gmail.com>
2025-11-21 12:27:13 +0000

    style: add semicolons to statements
    - Add semicolons to the end of statements in various files.
    - Ensures consistent code formatting across the project.
    - Aligns with common TypeScript/JavaScript style guides.
    - No functional changes introduced by this commit.

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -2,19 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { query } from "infra/database";
 import { Database } from "lucide-react";
 async function status(request: NextApiRequest, response: NextApiResponse) {
+  const dbName = process.env.POSTGRES_DB;
+  const maxDatabaseOpenedConnectionsQuery = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const databaseOpenedConnectionsResult = maxDatabaseOpenedConnectionsQuery.rows[0].count;
 
-  const dbName = process.env.POSTGRES_DB
-  const maxDatabaseOpenedConnectionsQuery = await query({ text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", values: [dbName] })
-  const databaseOpenedConnectionsResult = maxDatabaseOpenedConnectionsQuery.rows[0].count
-
-  const databaseMaxConnectionsResult = await query(`SHOW max_connections;`)
-  const maxConnectionsResult = parseInt(databaseMaxConnectionsResult.rows[0].max_connections)
+  const databaseMaxConnectionsResult = await query(`SHOW max_connections;`);
+  const maxConnectionsResult = parseInt(databaseMaxConnectionsResult.rows[0].max_connections);
 
   response.status(200).json({
     updated_at: new Date().toISOString(),
     max_connections: maxConnectionsResult,
     opened_connections: databaseOpenedConnectionsResult,
-
   });
 }
 
